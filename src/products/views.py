cee44b3d24f3495447046b793b259058fb1ce18b
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-07-29 23:33:55 +0300

    Summary

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import ProductCategory, Product
+
 
 # Create your views here.
 def index(request):
@@ -14,24 +16,7 @@ def products(request):
 
     context = {
         'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all()
     }
     return render(request, 'products/products.html', context)
-    
-    
-def test_context(request):
-
-    context = {
-        'title': 'store',
-        'header': 'Добро пожаловать!',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи чёрного цвета с монограммами "Adidas Originals"', 'price': 6090.00},
-            {'name': 'Синяя куртка "The North Face"', 'price': 23725.00},
-            {'name': 'Коричневый спортивный "oversized-топ ASOS DESIGN"', 'price': 3390.00},
-        ],
-        'promotion': True,
-        'products_of_promotion': [
-            {'name': 'Чёрный рюкзак "Nike Heritage"', 'price': 2340.00},
-        ]
-    }
-    return render(request, 'products/test-context.html', context)
