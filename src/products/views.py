0b3efd036438294a541d96cb5bdb5deca023771d
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-08-25 21:57:47 +0300

    Summary

--- src/products/views.py
+++ src/products/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 
 from django.core.paginator import Paginator
 
+from django.views.generic import ListView
+from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
@@ -38,6 +41,25 @@ def products(request, category_id=None, page=1):
         'products': products_paginator
     })
     return render(request, 'products/products.html', context)
+    
+    
+class SearchResultsView(ListView):
+
+    model = Product
+    context_object_name = 'object_list'
+    template_name = 'products/search_results.html'
+    
+    def get_queryset(self):
+        search_query = self.request.GET.get('q')
+        print(F'{search_query}')
+        object_list = Product.objects.filter(
+            Q(name__icontains=search_query) | Q(description__icontains=search_query) | Q(short_description__icontains=search_query)
+        )
+        if Product.objects.filter(name__icontains=search_query).exists():
+            print(F'{object_list}')
+        else:
+            print("Товара(-ов) с таким ***[ НАЗВАНИЕМ ]*** нет!")
+        return object_list
     
 
 @login_required
